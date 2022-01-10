a0ff70c89d8a15628369230b1a40846dd3e9bd48
gabri <gabrielsouzafraga@outlook.com>
2022-01-09 22:24:55 -0300

    Projeto Spotify Clone V.1

---
+++ spotify-app-clone/atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+})
+
+export const PlaylistIdState = atom({
+  key: "playlistIdState",
+  default: "37i9dQZF1DWZv6UE8BMPO0",
+});
